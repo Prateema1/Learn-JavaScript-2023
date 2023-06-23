f05d3f706d9cecdc528b089c0b920692ceedcdc0
Pratima <amitrana@MacUser.local>
2023-06-23 17:29:43 +0530

    Example og Hoisting in JS

---
+++ index.js
@@ -0,0 +1,14 @@
+
+console.log(x);
+getGreeting();
+getName();
+
+var x = 10;
+function getGreeting () {
+    console.log("Hello World!");
+}
+
+var getName = () => {
+    console.log("Hello Jack!");
+}
+
